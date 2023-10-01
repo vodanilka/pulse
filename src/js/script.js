f3998419d4ea8c0115025aac6ddaee1c01a3711f
Danil <142069206+vodanilka@users.noreply.github.com>
2023-10-01 09:15:42 -0700

    add JS for card

--- src/js/script.js
+++ src/js/script.js
@@ -7,4 +7,27 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="icon/right.png"></button>',
       }
     );
+    
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+  
+  function toggleClass (item) {
+    $(item).each(function(i){
+      $(this).on('click', function(e){
+      e.preventDefault();
+      $('.catalog-item__content').eq(i).toggleClass('.catalog-item__content_active');
+      $('.catalog-item__list').eq(i).toggleClass('.catalog-item__list_active');
+      })
+    })
+  }
+  toggleClass('.catalog-item__link');
+  toggleClass('.catalog-item__back')
+  
+    
+
+ 
+    
   });
