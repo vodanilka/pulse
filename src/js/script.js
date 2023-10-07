ec99668392ae817cc906674c10c649bb9beb3143
Danil <142069206+vodanilka@users.noreply.github.com>
2023-10-06 18:50:42 -0700

    email script

--- src/js/script.js
+++ src/js/script.js
@@ -76,5 +76,25 @@ validateForm('#order form');
 
 $('input[name="phone"]').mask("(999) 999-9999");
 
+$('form').submit(function(e){
+  e.preventDefault();
 
+  if (!$(this).valid()){
+    return;
+  }
+
+  $.ajax({
+    type: "POST",
+    url: "mailer/smart.php",
+    data: $(this).serialize()
+  }).done(function(){
+    $(this).find("input").val("");
+    $('#consultation, #order').fadeOut();
+    $('.overlay, #thanks').fadeIn('slow');
+
+    $('form').trigger('reset');
+
+  });
+  return false;
+});
 })
